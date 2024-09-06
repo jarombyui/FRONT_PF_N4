63a38b7387bd95efed836fd3f27f600b6fd3e7cf
jarombyui <mcamposro93@ucvvirtual.edu.pe>
2024-09-06 12:31:09 -0500

    mejora de estilos y colores

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
               </li>
               
               <li className="mb-2">
-                <button className="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-200">
+                <button className="w-full text-left px-4 py-2 rounded-lg bg-gray-400 text-gray-700 hover:bg-gray-200">
                   <Link to="/reportes">
                     Mis reportes
                   </Link>
